d5d907bf937d9e355b830ad52061fde17fa0aad2
rkarmadhn <rakaaputtra1@gmail.com>
2022-12-19 19:48:29 +0700

    First Commit

---
+++ js/navigation-script.js
@@ -0,0 +1,24 @@
+const menuBtn = document.querySelector(".menu-icon span");
+const searchBtn = document.querySelector(".search-icon");
+const cancelBtn = document.querySelector(".cancel-icon");
+const items = document.querySelector(".nav-items");
+const form = document.querySelector(".form-request");
+
+menuBtn.onclick = () => {
+  items.classList.add("active");
+  menuBtn.classList.add("hide");
+  searchBtn.classList.add("hide");
+  cancelBtn.classList.add("show");
+};
+cancelBtn.onclick = () => {
+  items.classList.remove("active");
+  menuBtn.classList.remove("hide");
+  searchBtn.classList.remove("hide");
+  cancelBtn.classList.remove("show");
+  form.style.opacity = "0";
+};
+searchBtn.onclick = () => {
+  form.style.opacity = "1";
+  searchBtn.classList.add("hide");
+  cancelBtn.classList.add("show");
+};
