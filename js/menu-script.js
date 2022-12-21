559e5306e6b45806ad961716bffabfc696ed49d8
rkarmadhn <rakaaputtra1@gmail.com>
2022-12-21 15:11:32 +0700

    New Tab Menu

---
+++ js/menu-script.js
@@ -0,0 +1,30 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("menu-text");
+  let headSlides = document.getElementsByClassName("head-active");
+
+  if (n > slides.length && n > headSlides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    headSlides[i].style.background = "none";
+    headSlides[i].style.color = "#2a435d";
+}
+  slides[slideIndex-1].style.display = "block";
+  headSlides[slideIndex-1].style.background = "#cc3333";
+  headSlides[slideIndex-1].style.color = "#fff";
+}
+
