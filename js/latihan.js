dd686409b8315149f274a439262293b2c40a8dc0
rkarmadhn <rakaaputtra1@gmail.com>
2022-12-22 18:23:18 +0700

    Add section Delivey & Testimonial

---
+++ js/latihan.js
@@ -0,0 +1,67 @@
+// // // let myAge = prompt('Enter Age :');
+// // // let yourAge = prompt('Enter your age :');
+
+// // // if (myAge > yourAge){
+// // //     let a = myAge - yourAge;
+// // //     confirm('Umur saya lebih tua '+ a +' dari pada kamu');
+// // // }
+// // // if (myAge < yourAge) {
+// // //     let b = yourAge - myAge
+// // //     confirm('Umur kamu lebih tua '+ b +' dari pada saya');
+// // // }
+
+// // // Soal ke 2
+// // // let a = 1;
+// // // let b = 4;
+
+// // // if (a > b) {
+// // //   console.log(a + " is grether than " + b);
+// // // } else {
+// // //   console.log(a + " is less than " + b);
+// // // }
+
+// // // Soal ke 3
+// // // let a = prompt("Enter a number : ");
+
+// // // if (a % 2 == 0) {
+// // //   console.log(a + " is an even number.");
+// // // } else {
+// // //   console.log(a + " is an odd number.");
+// // // }
+
+// // Soal 1 Level 2
+// // let score = prompt('Masukan nilai anda : ')
+
+// // if (score >= 80 && score <= 100){
+// //     console.log('Your Score is A');
+// // } else if (score >= 70 && score <= 79){
+// //     console.log('Your Score is B');
+// // } else if (score >= 60 && score <= 69){
+// //     console.log('Your Score is C');
+// // } else if (score >= 50 && score <= 59){
+// //     console.log('Your Score is D');
+// // } else {
+// //     console.log('Your Score is E');
+// // }
+
+// const countries = ["Albania", "Bolivia", "Canada", "Denmark", "Ethiopia", "Finland", "Germany", "Hungary", "Ireland", "Japan", "Kenya"];
+// const webTechs = ["HTML", "CSS", "JavaScript", "React", "Redux", "Node", "MongoDB"];
+// const mernStack = ["MongoDB", "Express", "React", "Node"];
+
+// // for (let i = 0; i <= 7; i++){
+// //     console.log(i);
+// // }
+// // for (let y = 10; y >= 0; y--){
+// //     console.log(y);
+// // }
+// // for(let i = 0; i <= 10; i++){
+// //     // console.log(i + ' x ' + i + ' = '+ i*i);
+// //     console.log(i + ' ' + i*i + ' ' + i*i*i);
+// // }
+// for(let i = 0; i <= 100; i++){
+//     if (i % 2 == 0){
+//         console.log(i + ' is Even Number ');
+//     } else {
+//         console.log(i + ' is Odd Number');
+//     }
+// }
